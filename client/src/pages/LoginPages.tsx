7613c3f6062e39dc9c89fb9cc593b53fcd7e058c
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-05-04 17:20:39 +0300

    files updated

--- client/src/pages/LoginPages.tsx
+++ client/src/pages/LoginPages.tsx
@@ -1,41 +1,43 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { mockUsers } from "../data/usersData";
+import "../styles/Login.css";
 
 function LoginPages() {
-  const [id, setId] = useState(0);
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
-  function checkUser(id: number, username: string): boolean {
+  function checkUser(username: string, password: string): boolean {
     const isUser = mockUsers.find(
-      (user) => user.id === id && user.username === username,
+      (user) => user.username === username && user.password === password,
     );
     return isUser !== undefined;
   }
-  function handleSubmit(e: React.FormEvent) {
+  function handleSubmit(e: { preventDefault: () => void }) {
     e.preventDefault();
-    const isValid = checkUser(id, username);
+    const isValid = checkUser(username, password);
     setMessage(isValid ? "Login successful!" : "Invalid ID or username.");
   }
 
   return (
-    <>
+    <div className="login-form">
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="id"
-          onChange={(e) => setId(Number(e.target.value))}
-        />
         <input
           type="text"
           placeholder="username"
           onChange={(e) => setUsername(e.target.value)}
         />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <button type="submit">Login</button>
         {message && <p>{message}</p>}
       </form>
-    </>
+    </div>
   );
 }
 
