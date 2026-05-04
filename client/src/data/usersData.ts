5012328ade06a2e100b1483d57dd5aa2b2006f67
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-05-04 17:22:02 +0300

    updated data

--- client/src/data/usersData.ts
+++ client/src/data/usersData.ts
@@ -1,13 +1,14 @@
-type UserRole = 'Admin' | 'User';
+type UserRole = "Admin" | "User";
 
 export interface User {
   readonly id: number;
   username: string;
+  password: string;
   role: UserRole;
 }
 
 export const mockUsers: User[] = [
-  { id: 1, username: 'alice_dev', role: 'Admin' },
-  { id: 2, username: 'bob_manager', role: 'User' },
-  { id: 3, username: 'charlie_guest', role: 'User' },
+  { id: 1, username: "alice_dev", password:"alice123",role: "Admin" },
+  { id: 2, username: "bob_manager", password:"bob123",role: "User" },
+  { id: 3, username: "charlie_guest", password:"charlie123",role: "User" },
 ];
